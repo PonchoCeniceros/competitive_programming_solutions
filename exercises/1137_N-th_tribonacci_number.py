b511876685ebb7dc5babf6408f0a3a53c157562f
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2025-12-24 13:40:47 -0600

    .

---
+++ exercises/1137_N-th_tribonacci_number.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+
+#
+# Exercise 1137
+#
+class Solution:
+    memo: Dict = {}
+
+    def tribonacci(self, n: int) -> int:
+        if n < 2:
+            return n
+
+        if n == 2:
+            return 1
+
+        if n in self.memo:
+            return self.memo[n]
+
+        self.memo[n] = (
+            self.tribonacci(n - 3) + self.tribonacci(n - 2) + self.tribonacci(n - 1)
+        )
+        return self.memo[n]
+
+
+if __name__ == "__main__":
+    solver = Solution()
+    # print("✅", solver.)
---
+++ exercises/70_climbing_stairs.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+
+#
+# Exercise 70
+#
+class Solution:
+    memo: Dict = {}
+
+    def climbStairs(self, n: int) -> int:
+        # cuantas formas hay de bajar:
+        #
+        # 0 escalones: 1 forma, no hacer nada
+        # 1 escalón:   1 forma, subir el escalon
+        # 2 escalones: 2 formas,
+        #             (1) subir de a 2 escalones,
+        #             (2) subir de 1 en 1
+        if n < 3:
+            return n
+
+        if n in self.memo:
+            return self.memo[n]
+
+        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
+
+
+if __name__ == "__main__":
+    solver = Solution()
+    print("✅", solver.climbStairs(n=2))
+    print("✅", solver.climbStairs(n=3))
