bb17696e35631de50613239ea87dbe37fcf776ce
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2025-12-24 13:01:21 -0600

    .

---
+++ exercises/509_fibonacci_number.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+
+#
+# Exercise 509
+#
+class Solution:
+    memo: Dict = {}
+
+    def fib(self, n: int) -> int:
+        #
+        # caso(s) base
+        #
+        if n < 2:
+            return n
+
+        # veo si el resultado de este caso ya fué
+        # calculado por otra llamada recusiva previa
+        #
+        if n in self.memo:
+            return self.memo[n]
+
+        # calculo el resultado y lo almaceno en memoria
+        #
+        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.memo[n]
+
+
+if __name__ == "__main__":
+    solver = Solution()
+    print("✅", solver.fib(n=2))
+    print("✅", solver.fib(n=3))
+    print("✅", solver.fib(n=4))
