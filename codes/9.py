b65b159bc2977c40d24e1712b1fe20a64e347f05
Giovanny Alfonso Chávez Ceniceros <gio.icc.uach@gmail.com>
2026-02-02 15:27:35 -0600

    [main] codes/9

--- codes/9.py
+++ codes/9.py
@@ -7,7 +7,19 @@ from typing import List, Dict, Optional
 # Palindrome Number
 #
 class Solution:
-    def isPalindrome(self, x: int) -> bool: ...
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        n = [int(i) for i in str(x)]
+        r = n[::-1]
+
+        for i in range(len(n)):
+            # Log.magenta(f"{n[i]} {r[i]}")
+            if n[i] != r[i]:
+                return False
+
+        return True
 
 
 # Unit tests
