9070d7711a418fd81f1d9f25d2a0261e887ef56c
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2026-02-11 17:14:52 -0600

    [katas] 836

---
+++ katas/836.py
@@ -0,0 +1,23 @@
+import pytest
+from utils.log import Log
+from typing import List, Dict, Optional
+
+#
+# Rectangle Overlap
+#
+class Solution:
+    def solve(self, *args, **kwargs):
+        # Implementa tu solución aquí
+        pass
+
+# Unit tests
+
+@pytest.mark.parametrize(
+    "params, expected",
+    [
+        # (input, output),
+    ],
+)
+def test_rectangle_overlap(params, expected):
+    sol = Solution()
+    # assert sol.solve(params) == expected
