13a0dfb330728e1f44901207ae4d5d7386d15992
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2026-02-12 17:25:07 -0600

    [836]

--- katas/836.py
+++ katas/836.py
@@ -2,22 +2,27 @@ import pytest
 from utils.log import Log
 from typing import List, Dict, Optional
 
+
 #
 # Rectangle Overlap
 #
 class Solution:
-    def solve(self, *args, **kwargs):
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
         # Implementa tu solución aquí
         pass
 
+
 # Unit tests
 
+
 @pytest.mark.parametrize(
-    "params, expected",
+    "rec1, rec2, expected",
     [
-        # (input, output),
+        ([0, 0, 2, 2], [1, 1, 3, 3], True),
+        ([0, 0, 1, 1], [1, 0, 2, 1], False),
+        ([0, 0, 1, 1], [2, 2, 3, 3], False),
     ],
 )
-def test_rectangle_overlap(params, expected):
+def test_rectangle_overlap(rec1, rec2, expected):
     sol = Solution()
-    # assert sol.solve(params) == expected
+    assert sol.isRectangleOverlap(rec1, rec2) == expected
